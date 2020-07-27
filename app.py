902aa069649443b38bc6d85140451449b2bce8c7
junhchen11 <junhchen11@gmail.com>
2020-07-26 21:30:59 -0500

    added new sql endpoints

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
-from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -30,7 +29,32 @@ def select():
 
         row_dicts = [dict(row) for row in rows]
 
-        row_data = {"data": row_dicts, "length": len(row_dicts)}
+        row_data = {"data": row_dicts}
+        # return
+
+        return row_data
+
+    except Exception as e:
+
+        return {"message": dict(e)}, "hello sample"
+
+
+@app.route("/selectFacebook")
+def selectFacebook():
+    try:
+        # Query
+        result = db.engine.execute(
+            "SELECT url FROM wikiarticles WHERE name = 'Facebook';"
+        )
+        if result is None:
+
+            return {"message": "Invalid query."}
+        rows = result.fetchall()
+        # Parse Output
+
+        row_dicts = [dict(row) for row in rows]
+
+        row_data = {"data": row_dicts}
         # return
 
         return row_data
@@ -44,13 +68,24 @@ def select():
 def insert():
     try:
         # Query
-        result = db.engine.execute(
-            "INSERT INTO wikiarticles VALUES ('hello', 'hello', 2, 'hello')"
+        db.engine.execute(
+            "INSERT INTO wikiarticles VALUES ('https://en.wikipedia.org/wiki/Facebook', 'Facebook', 9000, 'Social media')"
         )
-        return result
-    except Exception as e:
+        return "insertion successful"
+    except:
+        return "insertion failed"
 
-        return "succeeded"
+
+@app.route("/delete")
+def delete():
+    try:
+        # Query
+        db.engine.execute(
+            "DELETE FROM wikiarticles WHERE URL = 'https://en.wikipedia.org/wiki/Facebook'"
+        )
+        return "deletion successful"
+    except:
+        return "deletion failed"
 
 
 if __name__ == "__main__":
