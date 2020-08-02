a0a59fd9f827e738f0504f30a4faca00eab235aa
arifs <arifms2@illinois.edu>
2020-08-02 09:17:23 -0500

    Converts all scientific paper data to a JSON

--- test3.py
+++ test3.py
@@ -1,3 +1,4 @@
+import json
 import re
 import requests
 import urllib.request
@@ -58,23 +59,54 @@ for bditag in soup.find_all('bdi'):
     bookId += 1
 
 print("\nList of scientific papers")
-scicount = 0
-scirefs = soup.find_all('cite', class_='citation journal cs1')
-titles = []
-for aTag in scirefs:
+sciCount = 0  # To count the number of papers cited
+sciRefs = soup.find_all('cite', class_='citation journal cs1')
+
+trueSciRefs = []  # This contains only the actual BeautifulSoup elements that contain genuine scientific papers.
+titles = []  # Array with titles of scientific papers. Same for authors and dates below.
+authors = []
+dates = []
+refID = []
+journals = []
+
+# Attributes needed: Title, Authors, RefId, WikiURL, Name, journal; need: institution, field, ID (DOI)
+for aTag in sciRefs:
     authorList = aTag.contents[0].string
-    if "(" in authorList:
-        scicount += 1
+    if aTag.findAll(text="doi"):
+        trueSciRefs.append(aTag)
+        sciCount += 1
         str1 = aTag.a.string
+        # Papers store their titles in one of two locations, need to check
         if str1 == 'doi' or str1 == 'arXiv' or str1 == 'Bibcode':
             authorL = aTag.contents[0].string
             title = re.split('(")', authorL)
             titles.append(title[2])
         else:
             titles.append(str1)
-        sciDate = re.search(r'\((.*?)\)', authorList).group(1)
+        sciDate = re.search(r'\((.*?)\)', authorList).group(1)  # Regex to get the date which is enclosed in parentheses
         sciAuthors = authorList.split('(')
         sciAuthors = sciAuthors[0]
-        hashCode = hash(titles[scicount - 1])
-        print(str(scicount) + ": Title: " + titles[scicount - 1] + "; Published on: " + str(
+        hashCode = hash(titles[sciCount - 1])
+        authors.append(sciAuthors)
+        dates.append(sciDate)
+        journals.append(aTag.i.string)
+        refID.append(hashCode)
+        print(str(sciCount) + ": Title: " + titles[sciCount - 1] + "; Published on: " + str(
             sciDate) + "; Authors: " + sciAuthors + "; RefID: " + str(hashCode))
+jsonSci = {}
+jsonSci['Scientific Papers'] = []
+for i in range (0, len(journals)):
+    jsonSci['Scientific Papers'].append({
+        'WikiURL': str(url),
+        'count': i + 1,
+        'Title': titles[i],
+        'Authors': authors[i],
+        'Published Date': dates[i],
+        'Journal': journals[i],
+        'refID': refID[i]
+    })
+
+json_obj = json.dumps(jsonSci, indent=4)
+
+with open('jsonSci.txt', 'w') as outfile:
+    outfile.write(json_obj)
