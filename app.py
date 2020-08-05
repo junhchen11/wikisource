be5119ed2da12f58ceacb1df864255e9cb26d61a
junhchen11 <junhchen11@gmail.com>
2020-08-05 01:01:39 -0500

    changes

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import os
 from flask import Flask
 from flask import request
 from flask_sqlalchemy import SQLAlchemy
-import os
+import spacy
+import en_core_web_sm
+nlp = en_core_web_sm.load()
+stop_words = nlp.Defaults.stop_words
 
 
 app = Flask(__name__)
@@ -41,7 +45,7 @@ def selectArticle(article):
     if request.method == "GET":
         try:
             data = article
-            query = "SELECT url FROM wikiarticles WHERE name = %s;"
+            query = "SELECT url, name FROM wikiarticles WHERE name = %s;"
             # Query
 
             result = db.engine.execute(query, data)
@@ -91,9 +95,134 @@ def insertArticle():
         elif data == "https://en.wikipedia.org/wiki/Artificial Intelligence":
             query = "INSERT INTO wikiarticles (url, name) VALUES (%s , 'Artificial Intelligence');"
         db.engine.execute(query, data)
-
     except:
         return "insertion Failed"
+
+
+@app.route("/books/<name>", methods=['GET'])
+def books(name):
+    try:
+        # Query
+        data = name
+        query = "SELECT isbn, title, b.url FROM books b JOIN wikiarticles w ON(b.url = w.url) WHERE name = %s;"
+        result = db.engine.execute(query, data)
+        if result is None:
+
+            return {"message": "Invalid query."}
+        rows = result.fetchall()
+        # Parse Output
+
+        row_dicts = [dict(row) for row in rows]
+
+        row_data = {"data": row_dicts}
+        # return
+
+        return row_data
+
+    except:
+
+        return "Select Failed"
+
+
+@app.route("/scipapers/<name>", methods=['GET'])
+def scipapers(name):
+    try:
+        # Query
+        data = name
+        query = "SELECT title, s.url FROM scipapers s JOIN wikiarticles w ON(s.url = w.url) WHERE name = %s"
+        result = db.engine.execute(query, data)
+        if result is None:
+
+            return {"message": "Invalid query."}
+        rows = result.fetchall()
+        # Parse Output
+
+        row_dicts = [dict(row) for row in rows]
+
+        row_data = {"data": row_dicts}
+        # return
+
+        return row_data
+
+    except:
+
+        return "Select Failed"
+
+
+@app.route("/extarticles/<name>", methods=['GET'])
+def extarticles(name):
+    try:
+        # Query
+        data = name
+        query = "SELECT articleurl, e.url FROM externalarticles e JOIN wikiarticles w ON (e.url = w.url) WHERE name = %s"
+        result = db.engine.execute(query, data)
+        if result is None:
+
+            return {"message": "Invalid query."}
+        rows = result.fetchall()
+        # Parse Output
+
+        row_dicts = [dict(row) for row in rows]
+
+        row_data = {"data": row_dicts}
+        # return
+
+        return row_data
+
+    except:
+
+        return "Select Failed"
+
+
+@app.route("/afunction/<aname>/<isbn>", methods=['GET'])
+def afunction(aname, isbn):
+    try:
+        # Query
+        data = (isbn, aname)
+        print(isbn, aname)
+        query = "SELECT b.title FROM books b JOIN wikiarticles w ON (b.url = w.url) WHERE isbn = %s AND w.name = %s;"
+        result = db.engine.execute(query, data)
+        docCanada = "Canada is a country in the northern part of North America. Its ten provinces and three territories extend from the Atlantic to the Pacific and northward into the Arctic Ocean, covering 9.98 million square kilometres (3.85 million square miles), making it the world's second-largest country by total area. Its southern and western border with the United States, stretching 8,891 kilometres (5,525 mi), is the world's longest bi-national land border. Canada's capital is Ottawa, and its three largest metropolitan areas are Toronto, Montreal, and Vancouver."
+        docFacebook = "Facebook(stylized as facebook) is an American online social media and social networking service based in Menlo Park, California and a flagship service of the namesake company Facebook, Inc. It was founded by Mark Zuckerberg, along with fellow Harvard College students and roommates Eduardo Saverin, Andrew McCollum, Dustin Moskovitz and Chris Hughes.The founders initially limited Facebook membership to Harvard students. Membership was expanded to Columbia, Stanford, and Yale before being expanded to the rest of the Ivy League, MIT, and higher education institutions in the Boston area, then various other universities, and lastly high school students. Since 2006, anyone who claims to be at least 13 years old has been allowed to become a registered user of Facebook, though this may vary depending on local laws. The name comes from the face book directories often given to American university students."
+        docArtificialIntelligence = "Artificial intelligence (AI), sometimes called machine intelligence, is intelligence demonstrated by machines, unlike the natural intelligence displayed by humans and animals. Leading AI textbooks define the field as the study of intelligent agents: any device that perceives its environment and takes actions that maximize its chance of successfully achieving its goals.[1] Colloquially, the term artificial intelligence is often used to describe machines (or computers) that mimic cognitive functions that humans associate with the human mind, such as learning and problem solving. As machines become increasingly capable, tasks considered to require intelligence are often removed from the definition of AI, a phenomenon known as the AI effect.[3] A quip in Tesler's Theorem says AI is whatever hasn't been done yet.[4] For instance, optical character recognition is frequently excluded from things considered to be AI,[5] having become a routine technology.[6] Modern machine capabilities generally classified as AI include successfully understanding human speech,[7] competing at the highest level in strategic game systems (such as chess and Go),autonomously operating cars, intelligent routing in content delivery networks, and military simulations."
+        docTwitter = "Twitter is an American microblogging and social networking service on which users post and interact with messages known as tweets. Registered users can post, like, and retweet tweets, but unregistered users can only read them. Users access Twitter through its website interface, through Short Message Service (SMS) or its mobile-device application software (app).[16] Twitter, Inc. is based in San Francisco, California, and has more than 25 offices around the world.[17] Tweets were originally restricted to 140 characters, but was doubled to 280 for non-CJK languages in November 2017.[18] Audio and video tweets remain limited to 140 seconds for most accounts."
+        if result is None:
+            return {"message": "Invalid query."}
+        rows = result.fetchall()
+        # Parse Output
+        row_dicts = [dict(row) for row in rows]
+        row_data = {"data": row_dicts}
+        # return
+        titleObject = row_data['data']
+        title = titleObject[0]['title']
+        if aname == "Canada":
+            doc = docCanada
+        elif aname == "Facebook":
+            doc = docFacebook
+        elif aname == "Twitter":
+            doc = docTwitter
+        elif aname == "ArtificialIntelligence":
+            doc = docArtificialIntelligence
+        print(title)
+        print(doc)
+        title = nlp(title)
+        doc = nlp(doc)
+        sum = 0
+        count = 0
+        for token in title:
+            for token1 in doc:
+                if token.is_punct is not True:
+                    if token not in stop_words and token1 not in stop_words:
+                        print(token)
+                        print(token1)
+                        sum += token1.similarity(token)
+                        count += 1
+
+        articleRating = sum/count
+        print(articleRating)
+        return str(articleRating)
+    except:
+        return "Select Failed"
 
 
 @app.route("/")
